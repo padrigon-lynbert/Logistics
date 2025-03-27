85e96cc341f082278059a38d52538292bb7a0978
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2025-03-27 15:17:05 +0800

    changes: submodules of /vehicle_reservation

--- vehicle_reservation/Templates/scheduling_and_availability/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def vehicle_availability_check(request):
-    return render(request, 'scheduling_and_availability/vehicle_availability_check.html')
-def recurring_reservations(request):
-    return render(request, 'scheduling_and_availability/recurring_reservations.html')
-def time_slot_management(request):
-    return render(request, 'scheduling_and_availability/time_slot_management.html')
--- vehicle_reservation/urls.py
+++ vehicle_reservation/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
-from .Templates.user_management.views import booking_history, profile_management, user_roles_and_permissions
-from .Templates.scheduling_and_availability.views import recurring_reservations, time_slot_management, vehicle_availability_check
-from .Templates.payment_and_billing.views import payment_processing, payment_verification, pricing_and_fare_calculation
+from .Templates.reservation_management.views import booking_requests, cancelation_and_modifications, reservation_approval
+from .Templates.vehicle_allocation_and_tracking.views import fleet_assignment, usage_reports, vehicle_status_monitoring
+from .Templates.compliance_and_maintenance_integration.views import inspection_scheduling, repair_and_maintenance_request, usage_compliance
 
 urlpatterns = [
-    path('vehicle_reservation/user_management/booking_history/', booking_history, name='booking_history'),
-    path('vehicle_reservation/user_management/profile_management/', profile_management, name='profile_management'),
-    path('vehicle_reservation/user_management/user_roles_and_permissions/', user_roles_and_permissions, name='user_roles_and_permissions'),
+    path('vehicle_reservation/user_management/booking_requests/', booking_requests, name='booking_requests'),
+    path('vehicle_reservation/user_management/cancelation_and_modifications/', cancelation_and_modifications, name='cancelation_and_modifications'),
+    path('vehicle_reservation/user_management/reservation_approval/', reservation_approval, name='reservation_approval'),
 
-    path('vehicle_reservation/scheduling_and_availability/recurring_reservations/', recurring_reservations, name='recurring_reservations'),
-    path('vehicle_reservation/scheduling_and_availability/time_slot_management/', time_slot_management, name='time_slot_management'),
-    path('vehicle_reservation/scheduling_and_availability/vehicle_availability_check/', vehicle_availability_check, name='vehicle_availability_check'),
+    path('vehicle_reservation/scheduling_and_availability/vehicle_status_monitoring/', vehicle_status_monitoring, name='vehicle_status_monitoring'),
+    path('vehicle_reservation/scheduling_and_availability/fleet_assignment/', fleet_assignment, name='fleet_assignment'),
+    path('vehicle_reservation/scheduling_and_availability/usage_reports/', usage_reports, name='usage_reports'),
 
-    path('vehicle_reservation/payment_and_billing/payment_processing/', payment_processing, name='payment_processing'),
-    path('vehicle_reservation/payment_and_billing/payment_verification/', payment_verification, name='payment_verification'),
-    path('vehicle_reservation/payment_and_billing/pricing_and_fare_calculation/', pricing_and_fare_calculation, name='pricing_and_fare_calculation'),
+    path('vehicle_reservation/payment_and_billing/inspection_scheduling/', inspection_scheduling, name='inspection_scheduling'),
+    path('vehicle_reservation/payment_and_billing/repair_and_maintenance_request/', repair_and_maintenance_request, name='repair_and_maintenance_request'),
+    path('vehicle_reservation/payment_and_billing/usage_compliance/', usage_compliance, name='usage_compliance'),
 
 
 ]
