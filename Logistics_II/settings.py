50eea775ce6a6b7c451533a5065120bc982e8de4
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-04-22 00:30:53 +0800

    ++ vendor activation

--- Logistics_II/settings.py
+++ Logistics_II/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_init',
-    # 'vendor_portal',
+    'vendor_portal',
     # 'audit_management',
     # 'vehicle_reservation',
     # 'fleet_management',
