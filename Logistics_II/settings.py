dcce1d869c5b719246164b2081a2ade826296bcd
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-04-21 22:39:11 +0800

    test prod

--- Logistics_II/settings.py
+++ Logistics_II/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Templates', 'supporting_templates')  # Adjust path based on your structure
+    os.path.join(BASE_DIR, 'templates', 'supporting_templates')  # Adjust path based on your structure
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
