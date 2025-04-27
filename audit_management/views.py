f0326e7681502cf5aff683ca58e64d9f23e98356
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-04-27 08:44:15 +0800

    edit audit

--- audit_management/views.py
+++ audit_management/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 
-def audit_objective(request):
-    return render(request, 'audit_planning_and_scheduling/audit_objective.html')
-def scope_of_the_audit(request):
-    return render(request, 'audit_planning_and_scheduling/scope_of_the_audit.html')
-def communication_plan(request):
-    return render(request, 'audit_planning_and_scheduling/communication_plan.html')
+# def audit_objective(request):
+#     return render(request, 'audit_planning_and_scheduling/audit_objective.html')
+# def scope_of_the_audit(request):
+#     return render(request, 'audit_planning_and_scheduling/scope_of_the_audit.html')
+# def communication_plan(request):
+#     return render(request, 'audit_planning_and_scheduling/communication_plan.html')
 
-def policies(request):
-    return render(request, 'compliance_management/policies.html')
-def procedures(request):
-    return render(request, 'compliance_management/procedures.html')
-def audits(request):
-    return render(request, 'compliance_management/audits.html')
+# def policies(request):
+#     return render(request, 'compliance_management/policies.html')
+# def procedures(request):
+#     return render(request, 'compliance_management/procedures.html')
+# def audits(request):
+#     return render(request, 'compliance_management/audits.html')
 
 def sales_performance(request):
     return render(request, 'reporting_analytics/sales_porformance.html')
