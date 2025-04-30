1d07ca91db1ba0213f1448ced3dba1d47b978308
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-04-30 10:43:37 +0800

    fix: market remove:app_init(app)

--- app_init/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import app_init
-
-# urlpatterns = [
-#     path('', app_init, name='dashboard')
-# ]
--- app_init/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-# def app_init(request):
-#     return render(request, 'Dashboard.html')
-
