90985577d82a31cfa01731c72aab973c0cdd339d
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-05-11 11:44:47 +0800

    removing url pathing

--- Logistics_II/middleware/block_urls.py
+++ Logistics_II/middleware/block_urls.py
@@ -1,14 +1,17 @@
+import logging
 from django.http import HttpResponseForbidden
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 class BlockAllDirectURLMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Don't block direct access in development
         if settings.DEBUG:
             return self.get_response(request)
 
-        # If not in DEBUG mode, block all direct URL access
+        logger.info("BlockAllDirectURLMiddleware triggered!")  # Log to check if it's being triggered
+
         return HttpResponseForbidden("Direct URL access is blocked.")
