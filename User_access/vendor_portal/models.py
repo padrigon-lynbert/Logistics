44330cd8382d2a6576902344661a575c755d44eb
Padrigon, Lynbert <padrigonlynbert@gmail.com>
2025-05-16 20:11:57 +0800

    fix models

--- User_access/vendor_portal/models.py
+++ User_access/vendor_portal/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 class Vendor(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    info = models.CharField(max_length=255)
+    info = models.CharField(max_length=300)
+    password = models.CharField(max_length=255)
+    bid = models.CharField(max_length=255)
+    img = models.BinaryField()
 
     class Meta:
-        db_table = 'user_info'  # This matches the table in the database
+        db_table = 'market_userinfo'
 
     def __str__(self):
         return f"{self.name} ({self.email})"
