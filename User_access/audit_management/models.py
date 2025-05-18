6e9b8fbccc80c6cd109bbdf25c4f4cb390596963
SuspiciousApplePie <andreiusita6@gmail.com>
2025-05-18 13:00:39 +0800

    feat: Add model for scheduling table

--- User_access/audit_management/models.py
+++ User_access/audit_management/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Audit(models.Model):
+    """Record for audit transaction."""
     audit_id = models.AutoField(primary_key=True)
     organization_id = models.IntegerField()
     title = models.CharField(max_length=256)
@@ -27,6 +28,36 @@ class Audit(models.Model):
                               choices=Status.choices,
                               default=Status.PLANNED)
     scheduled_date = models.DateField()
-    created_by = models.IntegerField()
+    created_by = models.IntegerField() # This is supposed to be a foreign key, subjected for changes.
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns audit title."""
+        return self.title
+
+class Scheduling(models.Model):
+    schedule_id = models.AutoField(primary_key=True)
+    audit_id = models.ForeignKey(Audit.audit_id, on_delete=models.CASCADE)
+    schedule_start = models.DateField()
+    schedule_end = models.DateField()
+    location = models.CharField(max_length=255)
+    time_zone = models.CharField(max_length=50)
+    
+    # Status types for schedule
+    class Status(models.TextChoices):
+        SCHEDULED = 'SCHED', 'Scheduled'
+        RESCHED = 'RESCH', 'Rescheduled'
+        CANCELLED = 'CNCLD', 'Cancelled'
+        COMPLETED = 'CMPLT', 'Completed'
+
+    status = models.CharField(max_length=5,
+                              choices=Status.choices,
+                              default=Status.SCHEDULED)
+    notes = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns schedule start."""
+        return self.schedule_start
