4a62b4165e45c09aa79ae0a6647c9f734bf353d8
SuspiciousApplePie <andreiusita6@gmail.com>
2025-05-20 13:42:15 +0800

    feat: Add delete url

--- Admin_access/audit_management/urls.py
+++ Admin_access/audit_management/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import policies, procedures, audits, edit_audit
+from .views import policies, procedures, audits, edit_audit, delete_audit
 from .views import audit_findings, social_media_activity, user_engagement
 from .views import audit_objective, communication_plan, scope_of_the_audit
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('audit/compliance_management/procedures/', procedures, name='procedures'),
     path('audit/compliance_management/audits/', audits, name='audits'),
     path('audit/compliance_management/edit_audit/<int:audit_id>/', edit_audit, name='edit_audit'),
+    path('audit/compliance_management/delete_audit/<int:audit_id>/', delete_audit, name='delete_audit'),
 
     path('audit/reporting_analytics/audit_findings/', audit_findings, name='audit_findings'),
     path('audit/reporting_analytics/social_media_activity/', social_media_activity, name='social_media_activity'),
