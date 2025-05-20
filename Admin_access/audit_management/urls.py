938c2157f38e3e1360578fcb4e1cd8d6e9135e28
SuspiciousApplePie <andreiusita6@gmail.com>
2025-05-20 11:49:06 +0800

    feat: Add url for edit_audit page

--- Admin_access/audit_management/urls.py
+++ Admin_access/audit_management/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import policies, procedures, audits
+from .views import policies, procedures, audits, edit_audit
 from .views import audit_findings, social_media_activity, user_engagement
 from .views import audit_objective, communication_plan, scope_of_the_audit
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('audit/compliance_management/policies/', policies, name='policies'),
     path('audit/compliance_management/procedures/', procedures, name='procedures'),
     path('audit/compliance_management/audits/', audits, name='audits'),
+    path('audit/compliance_management/edit_audit/<int:audit_id>/', edit_audit, name='edit_audit'),
 
     path('audit/reporting_analytics/audit_findings/', audit_findings, name='audit_findings'),
     path('audit/reporting_analytics/social_media_activity/', social_media_activity, name='social_media_activity'),
